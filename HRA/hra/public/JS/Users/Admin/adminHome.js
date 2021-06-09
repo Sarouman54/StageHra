02f84e61207cce1030a0a89b54a65f2ea5cd5c66
SueurMelissa <melgsxr2502@gmail.com>
2021-06-09 09:31:54 +0200

    evolution marqueur

--- HRA/hra/public/JS/Users/Admin/adminHome.js
+++ HRA/hra/public/JS/Users/Admin/adminHome.js
@@ -1,3 +1,5 @@
+	/* ******************************************	Création de la carte	********************************************* */
+
 var map = new ol.Map({
     target: 'map',
     layers: [
@@ -5,14 +7,21 @@ var map = new ol.Map({
         	source: new ol.source.OSM()
       	})
     ],
+	controls: ol.control.defaults().extend([
+		new ol.control.ScaleLine()
+	]),
     view: new ol.View({
       center: ol.proj.fromLonLat([2, 47]),
-      zoom: 5
+      zoom: 1
     })
 });
 
+	/* ******************************************************************************************************************** */
+
+	/* *******************************************	Actualisation de la carte	******************************************* */
+
 AjaxCall();
-setInterval(AjaxCall, 10000);
+var idInter = setInterval(AjaxCall, 10000);
 
 function AjaxCall(){
 	$.get(
@@ -23,19 +32,27 @@ function AjaxCall(){
 	)
 }
 
+	/* ******************************************************************************************************************** */
+
+	/* *********************************************	Ajout des points	*********************************************** */
+
 function AddMap(data) {
-	console.log(data)
+
 	if(map.getLayers().getLength() >= 2){
-		for(let i = 0; i+1 < map.getLayers().getLength() ; i++)
-			map.removeLayer(map.getLayers().item(i+1));
+		for(let i = 1; i <= map.getLayers().getLength() ;i++)
+			map.removeLayer(map.getLayers().item(i));
 	}
-		
-	data.forEach((coordinate) => {
-		var layer = new ol.layer.Vector({					
+	console.log(data)
+	data.forEach((dataObject) => {
+		var layer = new ol.layer.Vector({
+			id: dataObject["idTruck"],
+			heure: dataObject["heure"],
+			minute: dataObject["minute"],
+			seconde: dataObject["seconde"],				
 			source: new ol.source.Vector({
 				features: [
 					new ol.Feature({
-		  				geometry: new ol.geom.Point(ol.proj.fromLonLat([coordinate["longitude"], coordinate["latitude"]])),
+		  				geometry: new ol.geom.Point(ol.proj.fromLonLat([dataObject["longitude"], dataObject["latitude"]])),
 					}),
 				]
 			})
@@ -44,3 +61,67 @@ function AddMap(data) {
 	})
 };
 
+	/* ********************************************************************************************************************** */
+
+	/* *****************************************	Création fenêtre PopUp		********************************************* */
+
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
+
+var overlay = new ol.Overlay({
+	element: container,
+	autoPan: true,
+	autoPanAnimation: {
+		duration: 250
+	}
+});
+map.addOverlay(overlay);
+
+closer.onclick = function(){
+	overlay.setPosition(undefined);
+	closer.blur();
+	return false;
+};
+
+map.on('click', function(event){
+	map.forEachFeatureAtPixel(event.pixel,
+		function (feature){
+			if(feature){
+				var coordinate = event.coordinate;
+				var idTruck = GetLayerEvent(feature).get("id")
+				var heure = GetLayerEvent(feature).get("heure")
+				var minute = GetLayerEvent(feature).get("minute")
+				var seconde = GetLayerEvent(feature).get("seconde")
+				var data = "idTruck="+ idTruck.toString();
+				console.log(data)
+				clearInterval(idInter)
+				$.get(
+					'/mapTruck',
+					data, 
+				    AddMap, 
+					'json'
+				)
+				content.innerHTML = 'Camion' + idTruck.toString() + '<br> Heure: <br>' + heure.toString() + ' : ' + minute.toString() + ' : ' + seconde.toString();
+				overlay.setPosition(coordinate);
+			}
+			else{
+				overlay.setPosition(undefined);
+				closer.blur();
+			}
+		}
+	);
+});
+
+	/* *******************************************		Récupération des points		***************************************** */
+
+function GetLayerEvent(feature){
+	var layerMap = map.getLayers();
+	for(let i=1 ; i < layerMap.getLength(); i++){
+		var featureLayer = layerMap.item(i).getSource().getFeatures()[0];
+		if(feature === featureLayer){return layerMap.item(i)}
+	}
+	return null;
+}
+
+	/* ********************************************************************************************************************** */
--- HRA/hra/src/Controller/MapController.php
+++ HRA/hra/src/Controller/MapController.php
@@ -11,48 +11,103 @@ use App\Entity\Truck;
 
 class MapController extends AbstractController
 {
+    
+    /**************************************************************    Affichage camions    *************************************************************/
+    
     #[Route('/adminMap', name: 'map')]
     public function map(): Response
     {
         $doctrine = $this->getDoctrine()->getManager();    
         $truckObjects = $doctrine->getRepository(Truck::class)->findAll();    
-        $coordinate = array();
+        $truckCoordinate = array();
         foreach($truckObjects as $truckObject){
+            
             $dataArray = $truckObject->getIdData();
             $dataObject = $dataArray->last();
 
-            
-            $nmeaTrame = explode(",", $dataObject->getNmea());
-            
-            $latitude = floatval($nmeaTrame[2]);
-            $longitude = floatval($nmeaTrame[4]);
-            
-            
-            $degreLatitude = ($latitude/100)%100;
-            $minuteLatitude = $latitude%100;
-            $secondeLatitude = $latitude-($latitude%10000);
-            $trameLatitude = $degreLatitude + ($minuteLatitude/60)+ ($secondeLatitude*60/3600);
-       
-            $degreLongitude = ($longitude/100)%1000;
-            $minuteLongitude = $longitude%100;
-            $secondeLongitude = $longitude-($longitude%10000);
-            $trameLongitude = $degreLongitude + ($minuteLongitude/60)+ ($secondeLongitude*60/3600);
-            
-
-            $var = ($nmeaTrame[3]);
-            
-            if($var=='S'){
-                $trameLatitude = -$trameLatitude;
-            }
-            
-            $var2 = ($nmeaTrame[5]);
-            
-            if($var2=='O'){
-                $trameLongitude = -$trameLongitude;
-            }
-           array_push($coordinate, array("latitude" => $trameLatitude, "longitude" => $trameLongitude));
+            $heure = $this->Heure($dataObject->getNmea());
+            $coordinate = $this->DSMToDD($dataObject->getNmea());
+            array_push($truckCoordinate, array("idTruck" => $truckObject->getId(),  "latitude" => $coordinate["latitude"], "longitude" => $coordinate["longitude"], "heure" => $heure["heure"], "minute" => $heure["minute"], "seconde" => $heure["seconde"]));
         }
         
-        return new JsonResponse($coordinate);
+        return new JsonResponse($truckCoordinate);
     }
+    
+    /****************************************************************************************************************************************************/
+    
+    /**********************************************************    Affichage camions => ID    ***********************************************************/    
+    
+    #[Route('/mapTruck', name: 'mapTruck')]
+    public function sendDataIdTruck(): Response
+    {    
+        if(!isset($_GET["idTruck"]))
+            return new Response(Response::HTTP_NOT_FOUND);
+    
+        $doctrine = $this->getDoctrine()->getManager();
+        $truckObject = $doctrine->getRepository(Truck::class)->findOneById($_GET["idTruck"]);
+        $truckCoordinate = array();
+        
+        foreach($truckObject->getIdData() as $dataObject) {            
+            $heure = $this->Heure($dataObject->getNmea());
+            $coordinate = $this->DSMToDD($dataObject->getNmea());
+            array_push($truckCoordinate, array("idTruck" => $truckObject->getId(),  "latitude" => $coordinate["latitude"], "longitude" => $coordinate["longitude"], "heure" => $heure["heure"], "minute" => $heure["minute"], "seconde" => $heure["seconde"]));
+        }
+        
+        return new JsonResponse($truckCoordinate);
+    }
+
+    /****************************************************************************************************************************************************/    
+    
+    /********************************************************    Calcul Longitude et Latitude    ********************************************************/  
+    
+    private function DSMToDD($trameArray): array
+    {    
+        $nmeaTrame = explode(",", $trameArray);
+        
+        $latitude = floatval($nmeaTrame[2]);
+        $longitude = floatval($nmeaTrame[4]);
+        
+        $degreLatitude = ($latitude/100)%100;
+        $minuteLatitude = $latitude%100;
+        $secondeLatitude = $latitude-($latitude%10000);
+        $trameLatitude = $degreLatitude + ($minuteLatitude/60)+ ($secondeLatitude*60/3600);
+        
+        $degreLongitude = ($longitude/100)%1000;
+        $minuteLongitude = $longitude%100;
+        $secondeLongitude = $longitude-($longitude%10000);
+        $trameLongitude = $degreLongitude + ($minuteLongitude/60)+ ($secondeLongitude*60/3600);
+        
+        $var = ($nmeaTrame[3]);
+        
+        if($var=='S'){
+            $trameLatitude = -$trameLatitude;
+        }
+        
+        $var2 = ($nmeaTrame[5]);
+        
+        if($var2=='O'){
+            $trameLongitude = -$trameLongitude;
+        }
+        return array("longitude" => $trameLongitude, "latitude" => $trameLatitude);
+    }
+    
+    /****************************************************************************************************************************************************/
+
+    /*****************************************************************    Calcul Heure    ***************************************************************/
+    
+    private function Heure($trameArray): array
+    {
+        $nmeaTrame = explode(",", $trameArray);
+
+        $temps = floatval($nmeaTrame[1]);
+        
+        $heure = (($temps/10000)%100000);
+        $minute = (($temps/100)%100);
+        $seconde = ($temps%100);
+       
+        return array("heure" => $heure, "minute" => $minute, "seconde" => $seconde);
+    }
+    
+    /****************************************************************************************************************************************************/
+   
 }
